2dbd8692d386196b9247b82f0b2db8322413eeb8
Olivia Wermundsén <olivia@wermundsen.net>
2024-08-29 15:04:53 +0300

    Hover function

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-window.onload = function() {
+window.onload = function () {
     // Lyssna efter händels
+    //document.getElementsByClassName("profile")
+    const profiles = document.getElementsByClassName("profile");
+
+
+    //en for loop för att gå igenom alla bilder.
+    for (let i = 0; i < profiles.length; i++) {
+        const profile = profiles[i];
+
+        if (profile && profile.id) {
+            // mouseenter eventlistener
+            profile.addEventListener("mouseenter", function () {
+                effectOn(profile.id);
+            });
+
+            // mouseleave eventlistener
+            profile.addEventListener("mouseleave", function () {
+                effectOff(profile.id);
+            });
+        }
+
+    }
 }
 
 
-var editTitle = function(e) {
+var editTitle = function (e) {
     // Ändra "team-title"
 };
 
-function effectOn(id){
+
+
+
+function effectOn(id) {
     // Rita en ram runt personen, kanske lite drop shadow?
-    document.getElementsByClassName("profile").style.border = "2px solid #000";
+    document.getElementById(id).style.border = "2px solid #000";
 }
 
-function effectOff(id){
+function effectOff(id) {
     // Stäng av effekten när musen inte längre är ovanför personen
-    document.getElementsByClassName("profile").style.border = "none";
+    document.getElementById(id).style.border = "none";
 }
 
