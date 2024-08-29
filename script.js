8b20400ddeaceea1385c7a99015812f87d7563ae
Olivia Wermundsén <olivia@wermundsen.net>
2024-08-29 11:58:46 +0300

    föreläsning 1

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 window.onload = function() {
-    // Lyssna efter händelser
+    // Lyssna efter händels
 }
+
+
 var editTitle = function(e) {
     // Ändra "team-title"
 };
 
 function effectOn(id){
     // Rita en ram runt personen, kanske lite drop shadow?
+    document.getElementsByClassName("profile").style.border = "2px solid #000";
 }
 
 function effectOff(id){
     // Stäng av effekten när musen inte längre är ovanför personen
+    document.getElementsByClassName("profile").style.border = "none";
 }
+
